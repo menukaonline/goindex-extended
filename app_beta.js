88b657d62014f9e7be5b07a0778303d3d4f4d02c
cheems <xenonstaforums@gmail.com>
2021-09-02 20:32:58 +0530

    Fixed: File Size Calculation

--- app_beta.js
+++ app_beta.js
@@ -6359,12 +6359,12 @@ function utc2local(utc_datetime) {
   );
 }
 function formatFileSize(bytes) {
-  if (bytes >= 1000000000) {
-    bytes = (bytes / 1000000000).toFixed(2) + " GB";
-  } else if (bytes >= 1000000) {
-    bytes = (bytes / 1000000).toFixed(2) + " MB";
-  } else if (bytes >= 1000) {
-    bytes = (bytes / 1000).toFixed(2) + " KB";
+  if (bytes >= 1073741824) {
+    bytes = (bytes / 1073741824).toFixed(2) + " GB";
+  } else if (bytes >= 1048576) {
+    bytes = (bytes / 1048576).toFixed(2) + " MB";
+  } else if (bytes >= 1024) {
+    bytes = (bytes / 1024).toFixed(2) + " KB";
   } else if (bytes > 1) {
     bytes = bytes + " bytes";
   } else if (bytes == 1) {
