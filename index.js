4bfccf24cabb357922cf4a271cfe78b1dcdc99a4
cheems <xenonstaforums@gmail.com>
2021-10-22 14:12:37 +0530

    :bug: Fixed: Several Bugs

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ function html(current_drive_order = 0, model = {}) {
     window.current_drive_order = ${current_drive_order};
     window.UI = JSON.parse('${JSON.stringify(uiConfig)}');
   </script>
-  <script src="https://rawcdn.githack.com/cheems/goindex-extended/3c303c36ab8316f7dc660b5684d9283d89382a42/app.js"></script>
+  <script src="https://rawcdn.githack.com/cheems/goindex-extended/95d701c2c0cd3c54cfef8184ffc959e7435c9d2e/app.js"></script>
 </head>
 <body>
 </body>
